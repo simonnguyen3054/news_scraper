c627f0f313e233017144782412758ba12c62c36b
Simon Nguyen <simonnguyen3054@gmail.com>
2018-09-28 16:49:45 -0700

    add mongo

--- server.js
+++ server.js
@@ -1,34 +1,89 @@
 const cheerio = require('cheerio');
 const request = require('request');
+var mongojs = require('mongojs');
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+// Database configuration
+// Save the URL of our database as well as the name of our collection
+const databaseUrl = 'news';
+const collections = ['articles', 'comments'];
+
+// Use mongojs to hook the database to the db variable
+const db = mongojs(databaseUrl, collections);
+
+// This makes sure that any errors are logged if mongodb runs into an issue
+db.on('error', error => {
+  console.log('Database Error:', error);
+});
+
+app.get('/', function(req, res) {
+  res.send('Hello world');
+});
 
 request('https://news.bitcoin.com/', (error, response, body) => {
-
   if (error) console.log(error);
 
   let $ = cheerio.load(body);
   let results = [];
 
   $('div.td-module-thumb').each(function(i, element) {
-
-    let articleTitle = $(element).find('a').attr('title');
-    let articleLink = $(element).find('a').attr('href');
-    let articleSummary = $(element).siblings('.line-clamp').text();
+    let articleTitle = $(element)
+      .find('a')
+      .attr('title');
+    let articleLink = $(element)
+      .find('a')
+      .attr('href');
+    let articleSummary = $(element)
+      .siblings('.line-clamp')
+      .text();
     // console.log(articleSummary);
-    let imgSrc = $(element).find('img').attr('src');
+    let imgSrc = $(element)
+      .find('img')
+      .attr('src');
 
     if (imgSrc === undefined) {
-      imgSrc = $(element).find('span').attr('style');
+      imgSrc = $(element)
+        .find('span')
+        .attr('style');
       imgSrc = imgSrc.slice(21, imgSrc.length - 1);
     }
-    results.push({
-      artical: {
-        articleTitle: articleTitle,
-        articleLink: articleLink,
-        imgSrc: imgSrc,
-        articleSummary: articleSummary
-      }
-    });
 
+    results.push({
+      articleTitle: articleTitle,
+      articleLink: articleLink,
+      imgSrc: imgSrc,
+      articleSummary: articleSummary
+    });
   });
-  console.log(results);
+
+  results.forEach(articleScraped => {
+    db.articles.update(
+      { articleLink: articleScraped.articleLink },
+      {
+        articleTitle: articleScraped.articleTitle,
+        articleLink: articleScraped.articleLink,
+        imgSrc: articleScraped.imgSrc,
+        articleSummary: articleScraped.articleSummary
+      },
+      { upsert: true }
+    );
+  });
+});
+
+app.get('/articles', function(req, res) {
+  db.articles.find({}, function(error, found) {
+    if (error) {
+      console.log(error);
+    } else {
+      res.json(found);
+    }
+  });
+});
+
+// Set the app to listen on port 3000
+app.listen(3000, function() {
+  console.log('App running on port 3000!');
 });
